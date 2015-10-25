5ad7c0b8559a8009faedd44f2177610460777cee
John Ezekiel <jkezekiel@gmail.com>
2015-10-25 15:55:37 -0500

    layout formatting

--- resources/views/admin/calendar.blade.php
+++ resources/views/admin/calendar.blade.php
@@ -1,6 +1,9 @@
 @extends('admin/layout')
 @section('content')
-<div class="col-md-8">
+<div class="container">
+	<legend>Test</legend>
+</div>
+<div class="col-md-8 col-md-offset-2">
 	<div id="error"></div>
 	<div id="calendar"></div>
 </div>
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,9 +9,11 @@
 <body>
   <div id="url" style="display: none">{{url('')}}</div>
   @yield('content')
+
   <script src="{{ asset('/js/fullcalendar-2.3.1/lib/moment.min.js') }}"></script>
   <script src="{{ asset('/js/fullcalendar-2.3.1/lib/jquery.min.js') }}"></script>
   <script src="{{ asset('/js/fullcalendar-2.3.1/fullcalendar.min.js') }}"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
   <script src="{{ asset('/js/admin/main.js') }}"></script>
 </body>
 </html>
