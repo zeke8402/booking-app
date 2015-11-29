bc85fd934b8cbc6cd807f3474362b376d98a5052
John Ezekiel <jkezekiel@gmail.com>
2015-11-29 21:11:49 +0000

    adding calendar to availability

---
+++ public/js/admin/availability.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  var url = document.getElementById('url');
+  url = url.textContent;
+  var cDate = new Date();
+
+  // Calendar initialization
+  $('#calendar').fullCalendar({
+    editable: false,
+    header: {
+      left: 'prev,next today',
+      center: 'Appointments',
+      right: 'month, agendaWeek, agendaDay'
+    },
+    defaultDate: cDate,
+    defaultView: 'agendaWeek',
+    // API call returns a json feed
+    /*
+    events: {
+      url: url+'/api/get-all-appointments',
+      error: function() {
+        $('#error').html('Could not find any appointments');
+      }
+    },
+    */
+    
+    // Function to handle a day click event
+    dayClick: function(date, jsEvent, view) {
+      //$(this).css('background-color', 'red');
+    },
+    
+  });
+});
--- resources/views/admin/availability.blade.php
+++ resources/views/admin/availability.blade.php
@@ -1,5 +1,15 @@
 @extends('admin/layout')
 @section('content')
-<legend>Availability</legend>
-<p>There's nothing here...yet</p>
+<div class="col-lg-8">
+	<div id="error"></div>
+	<div id="calendar"></div>
+</div>
+<div class="col-lg-4">
+	<legend> Details </legend>
+	<div id="availability-details">
+		<p>Click on an appointment to show details.</p>
+	</div>
+</div>
+
+<script src="{{ asset('/js/admin/availability.js') }}"></script>
 @endsection
